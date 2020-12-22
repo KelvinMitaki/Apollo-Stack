b3a0db9b37da07c87dc0519734719c882cf77a2f
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-22 21:23:11 +0300

    added header logic

--- client/components/Homepage/Header.tsx
+++ client/components/Homepage/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "../../styles/home.module.css";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsHouseFill } from "react-icons/bs";
@@ -6,6 +6,19 @@ import { IoIosArrowDown } from "react-icons/io";
 
 const Header = () => {
   const [name, setName] = useState<string>("");
+  const searchDiv = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+  const handleClickOutside = (e: Event) => {
+    // @ts-ignore
+    if (searchDiv.current && !searchDiv.current.contains(e.target)) {
+      setName("");
+    }
+  };
   return (
     <div className={styles.header}>
       <div className={styles.search}>
@@ -21,26 +34,47 @@ const Header = () => {
         </div>
         <div className={styles.search_body}>
           <div className={styles.upper}>
-            <div>
+            <div
+              ref={searchDiv}
+              className={name === "forSale" ? styles.open : styles.close}
+              onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) =>
+                setName("forSale")
+              }
+            >
               <p>For Sale</p>
               <IoIosArrowDown size="2rem" />
             </div>
             <input type="text" />
-            <div>
+            <div
+              ref={searchDiv}
+              className={name === "price" ? styles.open : styles.close}
+              onClick={e => setName("price")}
+            >
               <p>Price</p>
               <IoIosArrowDown size="2rem" />
             </div>
           </div>
           <div className={styles.lower}>
-            <div onClick={e => console.log("focused")}>
+            <div
+              className={name === "category" ? styles.open : styles.close}
+              onClick={e => setName("category")}
+            >
               <p>category</p>
               <IoIosArrowDown size="2rem" />
             </div>
-            <div>
+            <div
+              ref={searchDiv}
+              className={name === "bedrooms" ? styles.open : styles.close}
+              onClick={e => setName("bedrooms")}
+            >
               <p>Bedrooms</p>
               <IoIosArrowDown size="2rem" />
             </div>
-            <div>
+            <div
+              ref={searchDiv}
+              className={name === "bathrooms" ? styles.open : styles.close}
+              onClick={e => setName("bathrooms")}
+            >
               <p>Bathrooms</p>
               <IoIosArrowDown size="2rem" />
             </div>
