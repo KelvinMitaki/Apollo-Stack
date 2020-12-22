c7895d00f88f4da14d6d996b13e897f45a05c68f
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-22 20:04:25 +0300

    basic styling to search

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsHouseFill } from "react-icons/bs";
+import { IoIosArrowDown } from "react-icons/io";
 import styles from "../styles/home.module.css";
 
 export default function Home() {
@@ -17,6 +18,34 @@ export default function Home() {
             <div>
               <BsHouseFill size="3rem" />
               <p>House for sell</p>
+            </div>
+          </div>
+          <div className={styles.search_body}>
+            <div className={styles.upper}>
+              <div>
+                <p>For Sale</p>
+                <IoIosArrowDown size="2rem" />
+              </div>
+              <input type="text" />
+              <div>
+                <p>Price</p>
+                <IoIosArrowDown size="2rem" />
+              </div>
+            </div>
+            <div className={styles.lower}>
+              <div>
+                <p>category</p>
+                <IoIosArrowDown size="2rem" />
+              </div>
+              <div>
+                <p>Bedrooms</p>
+                <IoIosArrowDown size="2rem" />
+              </div>
+              <div>
+                <p>Bathrooms</p>
+                <IoIosArrowDown size="2rem" />
+              </div>
+              <button>Search</button>
             </div>
           </div>
         </div>
