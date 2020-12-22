d36cd669b8fd9d3124d52b0c158145dc2524ac80
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-22 18:23:59 +0300

    wired up search header

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,6 @@
 import Layout from "../components/Layout";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsHouseFill } from "react-icons/bs";
 import styles from "../styles/home.module.css";
 
 export default function Home() {
@@ -6,7 +8,18 @@ export default function Home() {
     <div className={styles.container}>
       <Layout title="Home Page" />
       <div className={styles.header}>
-        <div>Hello World</div>
+        <div className={styles.search}>
+          <div className={styles.search_head}>
+            <div>
+              <AiOutlineSearch size="3rem" />
+              <p>Find your home</p>
+            </div>
+            <div>
+              <BsHouseFill size="3rem" />
+              <p>House for sell</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
