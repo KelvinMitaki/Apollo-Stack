e12bc333d5496bdfc222c6258badfe7306e3a699
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-23 11:06:16 +0300

    added head layout

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import styles from "../styles/Head.module.css";
 
 interface Props {
   title: string;
@@ -7,10 +8,35 @@ interface Props {
 
 const Layout: React.FC<Props> = props => {
   return (
-    <Head>
-      <title>{props.title}</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <div>
+      <Head>
+        <title>{props.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.container}>
+        <div>
+          <p>yellow market</p>
+        </div>
+        <div>
+          <p>for sale</p>
+        </div>
+        <div>
+          <p>to rent</p>
+        </div>
+        <div>
+          <p>developments</p>
+        </div>
+        <div>
+          <p>repossessed</p>
+        </div>
+        <div>
+          <p>login</p>
+        </div>
+        <div>
+          <p>register</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
