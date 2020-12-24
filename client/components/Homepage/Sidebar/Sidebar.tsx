c92e22092439cceb6cd2c0b18fbaa535ce74d9e4
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-24 17:52:11 +0300

    wired up sidebar

--- client/components/Homepage/Sidebar/Sidebar.tsx
+++ client/components/Homepage/Sidebar/Sidebar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MdCancel } from "react-icons/md";
+import { StylingContext } from "../../../Context/StylingContext";
 import styles from "../../../styles/Layout.module.css";
 
 interface Props {
@@ -7,6 +9,7 @@ interface Props {
 }
 
 const Sidebar: React.FC<Props> = props => {
+  const { setToggle } = useContext(StylingContext);
   return (
     <div
       className={`${styles.sidebar} ${
@@ -14,7 +17,30 @@ const Sidebar: React.FC<Props> = props => {
       }`}
       ref={props.toggleRef}
     >
-      Sidebar
+      <div className={styles.sidebar_header}>
+        <h3>yellow market</h3>
+        <MdCancel size="3rem" onClick={() => setToggle && setToggle(false)} />
+      </div>
+      <div className={styles.sidebar_body}>
+        <div className={styles.opts_item}>
+          <p>for sale</p>
+        </div>
+        <div className={styles.opts_item}>
+          <p>to rent</p>
+        </div>
+        <div className={styles.opts_item}>
+          <p>developments</p>
+        </div>
+        <div className={styles.opts_item}>
+          <p>repossessed</p>
+        </div>
+        <div className={styles.opts_item}>
+          <p>login</p>
+        </div>
+        <div className={styles.opts_item}>
+          <p>register</p>
+        </div>
+      </div>
     </div>
   );
 };
