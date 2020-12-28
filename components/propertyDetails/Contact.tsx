3ce6684c7120d2f0f7b86e78db757f4d37870293
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-28 12:13:27 +0300

    scaffolded contact

--- components/propertyDetails/Contact.tsx
+++ components/propertyDetails/Contact.tsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 import styles from "../../styles/propertyDetails.module.css";
 
 const Contact = () => {
-  return <div>Contact Contact</div>;
+  return (
+    <div>
+      <div>
+        <h3>Contact Agent</h3>
+        <div>
+          <div>
+            <FaPhoneAlt />
+            <p>Contact Number</p>
+          </div>
+          <p>0712345678</p>
+        </div>
+        <div>
+          <div>
+            <MdEmail />
+            <p>Email Address</p>
+          </div>
+          <p>johndoe@gmail.com</p>
+        </div>
+      </div>
+      <div>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Phone Number" />
+        <input type="text" placeholder="Message" />
+        <button>Send Message</button>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
