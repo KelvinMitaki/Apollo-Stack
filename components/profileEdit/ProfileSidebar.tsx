175e87fffa9b554c25c41b4563c76654982d85f0
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-29 00:07:10 +0300

    scaffolded sidebar

--- components/profileEdit/ProfileSidebar.tsx
+++ components/profileEdit/ProfileSidebar.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { AiFillAlert } from "react-icons/ai";
+import { FiEdit } from "react-icons/fi";
+import { MdFavorite } from "react-icons/md";
 
 const ProfileSidebar = () => {
-  return <div>Sidebar Sidebar</div>;
+  return (
+    <div>
+      <div>
+        <FiEdit />
+        <p>Edit Profile</p>
+      </div>
+      <div>
+        <AiFillAlert />
+        <p>Alerts</p>
+      </div>
+      <div>
+        <MdFavorite />
+        <p>Favorites</p>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileSidebar;
