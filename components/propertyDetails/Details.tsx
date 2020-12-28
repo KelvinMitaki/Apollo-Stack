947d1e4dc311441fe8bf6f0e19bf9a91439b3afd
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-28 11:52:15 +0300

    wired up image slider

--- components/propertyDetails/Details.tsx
+++ components/propertyDetails/Details.tsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Slider from "react-slick";
 import styles from "../../styles/propertyDetails.module.css";
 
 const Details = () => {
+  let slider1 = useRef(null);
+  let slider2 = useRef(null);
+  const [nav, setNav] = useState<{
+    nav1: typeof slider1 | null;
+    nav2: typeof slider2 | null;
+  }>({ nav1: null, nav2: null });
+  useEffect(() => {
+    setNav({ nav1: slider1.current, nav2: slider2.current });
+  }, []);
   return (
     <div>
-      <div
-        className={styles.bg_image}
-        style={{ backgroundImage: "url(/image-2.jpg)" }}
-      ></div>
+      <Slider ref={slider1} asNavFor={nav.nav2 as any}>
+        <img src="/image-1.jpeg" alt="" className={styles.large_img} />
+        <img src="/image-2.jpg" alt="" className={styles.large_img} />
+        <img src="/image-3.jpg" alt="" className={styles.large_img} />
+      </Slider>
+      <Slider
+        asNavFor={nav.nav1 as any}
+        ref={slider2}
+        slidesToShow={3}
+        swipeToSlide
+        focusOnSelect
+        dots
+        autoplay
+        autoplaySpeed={5000}
+      >
+        <img src="/image-1.jpeg" alt="" className={styles.small_img} />
+        <img src="/image-2.jpg" alt="" className={styles.small_img} />
+        <img src="/image-3.jpg" alt="" className={styles.small_img} />
+      </Slider>
       <div>
-        <h4>Description</h4>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          cumque. Accusamus ex laudantium inventore aliquid, at autem eveniet
-          consequatur incidunt totam mollitia, in rem similique dignissimos non
-          modi dolorum id nisi aliquam temporibus nostrum. Totam velit mollitia
-          natus aut nam sapiente minima reiciendis! Temporibus inventore numquam
-          molestias delectus repudiandae magnam.
-        </p>
+        <div>
+          <h4>Description</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+            cumque. Accusamus ex laudantium inventore aliquid, at autem eveniet
+            consequatur incidunt totam mollitia, in rem similique dignissimos
+            non modi dolorum id nisi aliquam temporibus nostrum. Totam velit
+            mollitia natus aut nam sapiente minima reiciendis! Temporibus
+            inventore numquam molestias delectus repudiandae magnam.
+          </p>
+        </div>
         <div className={styles.table}>
           <div className={styles.table_title}>
             <h5>Overview</h5>
