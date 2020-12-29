5974a2934f0df81e9b8aaf6277302175f6c303b8
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-29 23:51:38 +0300

    wired up modal functionality

--- components/RegisterLogin/RegisterLoginModal.tsx
+++ components/RegisterLogin/RegisterLoginModal.tsx
@@ -1,17 +1,38 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Redux } from "../../interfaces/Redux";
+import { ActionTypes } from "../../redux/types/types";
 import styles from "../../styles/registerLoginModal.module.css";
+import { SetToggleLogin } from "../Layout/Layout";
 
 const RegisterLoginModal = () => {
+  const dispatch = useDispatch();
+  const loginRef = useRef<HTMLDivElement>(null);
   const toggleLogin = useSelector((state: Redux) => state.styling.toggleLogin);
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+  const handleOutsideClick = (e: Event) => {
+    // @ts-ignore
+    if (loginRef.current && !loginRef.current.contains(e.target)) {
+      dispatch<SetToggleLogin>({
+        type: ActionTypes.toggleLogin,
+        payload: false
+      });
+    }
+  };
   return (
     <div
       className={`${styles.container} ${
         toggleLogin ? styles.container__show : ""
       }`}
     >
-      RegisterLoginModal RegisterLoginModal
+      <div ref={loginRef} className={styles.body}>
+        RegisterLoginModal RegisterLoginModal
+      </div>
     </div>
   );
 };
