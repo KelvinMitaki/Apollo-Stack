bfd78e3da2166a38e4d9981145c7ad3a2b106c0f
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-29 17:00:06 +0300

    saved alerts basic structure

--- components/alerts/SavedAlerts.tsx
+++ components/alerts/SavedAlerts.tsx
@@ -8,30 +8,128 @@ const SavedAlerts = () => {
     <div>
       <h4>my saved alerts</h4>
       <div>
-        <div className={styles.header}>
-          <p>type</p>
-          <p>area</p>
-          <p>min price</p>
-          <p>max price</p>
-          <p>beds</p>
-          <p>baths</p>
-          <p>property type</p>
-          <p>created</p>
-        </div>
-        <div className={styles.body}>
-          <div className={styles.alert}>
-            <MdCancel />
-            <p>to rent</p>
+        <div className={styles.saved_alerts}>
+          <div className={styles.table}>
+            <p>type</p>
+            <p>area</p>
+            <p>min price</p>
+            <p>max price</p>
+            <p>beds</p>
+            <p>baths</p>
+            <p>property type</p>
+            <p>created</p>
+          </div>
+          <div className={styles.table}>
+            <div>
+              <MdCancel />
+              <p>to rent</p>
+            </div>
             <p>karen nairobi</p>
-            <p>ksh 150,000</p>
-            <p>ksh 200,000</p>
-            <p>any</p>
-            <p>any</p>
+            <p>ksh 150,000,000</p>
+            <p>ksh 200,000,000</p>
             <p>4+</p>
             <p>any</p>
             <p>apartment</p>
-            <p>2020-12-28</p>
-            <AiOutlineSearch />
+            <div>
+              <p>2020-12-28</p>
+              <AiOutlineSearch />
+            </div>
+          </div>
+          <div className={styles.table}>
+            <div>
+              <MdCancel />
+              <p>to rent</p>
+            </div>
+            <p>karen nairobi</p>
+            <p>ksh 150,000,000</p>
+            <p>ksh 200,000,000</p>
+            <p>4+</p>
+            <p>any</p>
+            <p>apartment</p>
+            <div>
+              <p>2020-12-28</p>
+              <AiOutlineSearch />
+            </div>
+          </div>
+          <div className={styles.table}>
+            <div>
+              <MdCancel />
+              <p>to rent</p>
+            </div>
+            <p>karen nairobi</p>
+            <p>ksh 150,000,000</p>
+            <p>ksh 200,000,000</p>
+            <p>4+</p>
+            <p>any</p>
+            <p>apartment</p>
+            <div>
+              <p>2020-12-28</p>
+              <AiOutlineSearch />
+            </div>
+          </div>
+          <div className={styles.table}>
+            <div>
+              <MdCancel />
+              <p>to rent</p>
+            </div>
+            <p>karen nairobi</p>
+            <p>ksh 150,000,000</p>
+            <p>ksh 200,000,000</p>
+            <p>4+</p>
+            <p>any</p>
+            <p>apartment</p>
+            <div>
+              <p>2020-12-28</p>
+              <AiOutlineSearch />
+            </div>
+          </div>
+          <div className={styles.table}>
+            <div>
+              <MdCancel />
+              <p>to rent</p>
+            </div>
+            <p>karen nairobi</p>
+            <p>ksh 150,000,000</p>
+            <p>ksh 200,000,000</p>
+            <p>4+</p>
+            <p>any</p>
+            <p>apartment</p>
+            <div>
+              <p>2020-12-28</p>
+              <AiOutlineSearch />
+            </div>
+          </div>
+          <div className={styles.table}>
+            <div>
+              <MdCancel />
+              <p>to rent</p>
+            </div>
+            <p>karen nairobi</p>
+            <p>ksh 150,000,000</p>
+            <p>ksh 200,000,000</p>
+            <p>4+</p>
+            <p>any</p>
+            <p>apartment</p>
+            <div>
+              <p>2020-12-28</p>
+              <AiOutlineSearch />
+            </div>
+          </div>
+          <div className={styles.table}>
+            <div>
+              <MdCancel />
+              <p>to rent</p>
+            </div>
+            <p>karen nairobi</p>
+            <p>ksh 150,000,000</p>
+            <p>ksh 200,000,000</p>
+            <p>4+</p>
+            <p>any</p>
+            <p>apartment</p>
+            <div>
+              <p>2020-12-28</p>
+              <AiOutlineSearch />
+            </div>
           </div>
         </div>
       </div>
