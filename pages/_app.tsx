63b01a4a65327a8a921afedca67cad8bce700d14
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-29 12:27:47 +0300

    wired up nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,12 @@ import { AppProps } from "next/app";
 import "../styles/globals.css";
 import "rc-slider/assets/index.css";
 import { wrapper } from "../redux/reducers";
+import nProgress from "nprogress";
+import Router from "next/router";
+
+(Router as any).onRouteChangeStart = () => nProgress.start();
+(Router as any).onRouteChangeComplete = () => nProgress.done();
+(Router as any).onRouteChangeError = () => nProgress.done();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
