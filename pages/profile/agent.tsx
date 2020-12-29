ca090270b9a176530e064efa1c44777255e0bd26
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-29 23:05:51 +0300

    fixed sticky footer

--- pages/profile/agent.tsx
+++ pages/profile/agent.tsx
@@ -5,7 +5,34 @@ import styles from "../../styles/agent.module.css";
 const agent = () => {
   return (
     <Layout title="Dashboard">
-      <div>agent</div>;
+      <div className={styles.container}>
+        <div>agent</div>;
+        <div>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic
+          veritatis similique sed, inventore voluptatem quo quos placeat
+          voluptates, unde quisquam nobis velit nesciunt officia fugiat aut
+          numquam labore! Unde saepe aliquid aut quae similique ullam at omnis
+          expedita accusantium eligendi nihil nesciunt eius, sequi ratione neque
+          vero quasi impedit facilis maiores? Aperiam voluptates laborum illum
+          animi magni id quos sed illo eveniet autem. Pariatur, excepturi hic!
+          Iste unde soluta illum fuga ullam autem illo quasi veniam accusantium
+          beatae inventore, quod labore deleniti! Totam id et distinctio
+          repellat amet itaque qui ducimus aspernatur illum similique incidunt
+          laboriosam iure, sequi voluptatum perferendis sunt unde quos, magni
+          praesentium! Alias commodi illum libero aperiam dolore! Veritatis ipsa
+          commodi eum voluptas eaque aliquid veniam! Unde eos vel laudantium,
+          officia voluptates neque facilis. Animi veritatis itaque ea nisi
+          libero quis, facilis aperiam explicabo placeat quidem ipsum quas
+          quibusdam. Ut recusandae sed officiis id molestiae, deleniti
+          dignissimos corrupti reprehenderit sapiente, quas quis atque, aliquid
+          aut magni nesciunt asperiores delectus iste itaque! Eius, eaque.
+          Repellendus aspernatur modi consequuntur quidem cum, porro error
+          adipisci cupiditate ducimus laudantium optio corrupti debitis fugiat,
+          sequi sapiente? Voluptas dolorem consequatur quae! Nesciunt repellat
+          laboriosam aspernatur explicabo asperiores tempora. Ipsam veritatis
+          officia libero expedita.
+        </div>
+      </div>
     </Layout>
   );
 };
