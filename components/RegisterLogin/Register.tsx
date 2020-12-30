75bfc44c2ae03b45b1bf6eccb6b20c5a6dfc5003
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-30 12:30:25 +0300

    scaffolded register

--- components/RegisterLogin/Register.tsx
+++ components/RegisterLogin/Register.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 import styles from "../../styles/registerLoginModal.module.css";
+import Input from "./Input";
 
 const Register = () => {
-  return <div className={styles.register}>Register Register</div>;
+  return (
+    <div className={styles.register}>
+      <Field component={Input} label="Full Name" type="text" name="fullName" />
+      <Field component={Input} label="Email" type="text" name="email" />
+      <Field
+        component={Input}
+        label="Password"
+        type="password"
+        name="password"
+      />
+      <Field
+        component={Input}
+        label="Confirm Password"
+        type="password"
+        name="confirmPassword"
+      />
+    </div>
+  );
 };
 
-export default Register;
+export default reduxForm({ form: "Register" })(Register);
