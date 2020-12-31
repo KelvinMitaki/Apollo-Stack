f0ebf5688d21be80183d3e9601cadebd0e072cbb
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-31 22:32:53 +0300

    added apollo boilerplate for ssr

---
+++ apollo.ts
@@ -0,0 +1,34 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject
+} from "@apollo/client";
+import { useMemo } from "react";
+
+let apolloClient: ApolloClient<NormalizedCacheObject>;
+
+const createApolloClient = () =>
+  new ApolloClient({
+    ssrMode: typeof window === "undefined",
+    cache: new InMemoryCache(),
+    uri: "http://localhost:4000/graphql"
+  });
+
+export const initializeApollo = (
+  initialState: NormalizedCacheObject | null = null
+) => {
+  const _apolloClient = apolloClient ?? createApolloClient();
+  if (initialState) {
+    _apolloClient.cache.restore(initialState);
+  }
+  if (typeof window === "undefined") {
+    return _apolloClient;
+  }
+  apolloClient = apolloClient ?? _apolloClient;
+  return apolloClient;
+};
+
+export const useApollo = (initialState: NormalizedCacheObject) => {
+  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+  return store;
+};
