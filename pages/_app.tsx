e61aec22eb44453fcf8b200713edb1a1c775d342
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-31 23:00:42 +0300

    added server side rendering fetching with apollo'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,23 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 import "rc-slider/assets/index.css";
+import moduleName, { ApolloProvider } from "@apollo/client";
 import { wrapper } from "../redux/reducers";
 import nProgress from "nprogress";
 import Router from "next/router";
+import { useApollo } from "../apollo";
 
 (Router as any).onRouteChangeStart = () => nProgress.start();
 (Router as any).onRouteChangeComplete = () => nProgress.done();
 (Router as any).onRouteChangeError = () => nProgress.done();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const client = useApollo(pageProps.initialApolloState);
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,40 @@
+import { gql, useQuery } from "@apollo/client";
+import { initializeApollo } from "../apollo";
 import Featured from "../components/Homepage/Featured/Featured";
 import Header from "../components/Homepage/Header/Header";
 import Layout from "../components/Layout/Layout";
 import styles from "../styles/home.module.css";
 
+const MyQuery = gql`
+  query {
+    users {
+      email
+      _id
+      fullName
+    }
+  }
+`;
+
 export default function Home() {
+  const { loading, data } = useQuery(MyQuery);
+  if (loading) return <div>Loading...</div>;
   return (
     <div className={`${styles.container}`}>
-      <Layout title="Home Page">
+      {/* <Layout title="Home Page">
         <Header />
         <Featured />
-      </Layout>
+      </Layout> */}
+      <pre style={{ fontSize: "2rem" }}>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const apolloClient = initializeApollo();
+  await apolloClient.query({ query: MyQuery });
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract()
+    }
+  };
+};
