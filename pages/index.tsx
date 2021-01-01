9e0e682a17a9338911660b63d6573eeb8f400ffe
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-01 12:47:20 +0300

    refactored

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,36 +5,16 @@ import Header from "../components/Homepage/Header/Header";
 import Layout from "../components/Layout/Layout";
 import styles from "../styles/home.module.css";
 
-const MyQuery = gql`
-  query {
-    users {
-      _id
-      fullName
-      email
-    }
-  }
-`;
-
 const Home: React.FC = props => {
-  const { data, loading } = useQuery(MyQuery);
-  if (loading) {
-    return <h2>loading...</h2>;
-  }
+  console.log(props);
   return (
     <div className={`${styles.container}`}>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Layout title="Home Page">
+        <Header />
+        <Featured />
+      </Layout>
     </div>
   );
-};
-
-export const getStaticProps = async () => {
-  const apolloClient = initializeApollo();
-  await apolloClient.query({ query: MyQuery });
-  return {
-    props: {
-      initialApolloState: apolloClient.cache.extract()
-    }
-  };
 };
 
 export default Home;
