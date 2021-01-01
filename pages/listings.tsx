297d91e0a57b0a1dcf06ad07d89d5d74eb3a250e
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-01 15:55:26 +0300

    fixed child occupy parent container

---
+++ pages/listings.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const listings = () => {
+  return (
+    <Layout title="Listings">
+      <div>listings listings</div>;
+    </Layout>
+  );
+};
+
+export default listings;
