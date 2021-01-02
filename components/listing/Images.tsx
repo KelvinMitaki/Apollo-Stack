c6bb4bde0eaf60fdf64b8b5e1344d4adfe44c032
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-03 01:29:59 +0300

    wired up images

--- components/listing/Images.tsx
+++ components/listing/Images.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styles from "../../styles/listingEdit.module.css";
 
 const Images = () => {
-  return <div>Images Images</div>;
+  const images = [] as JSX.Element[];
+  for (let i = 0; i < 50; i++) {
+    images.push(
+      <div
+        style={{ backgroundImage: "url(/image-3.jpg)" }}
+        className={styles.bg_image}
+      ></div>
+    );
+  }
+  return <div className={styles.Images}>{images}</div>;
 };
 
 export default Images;
