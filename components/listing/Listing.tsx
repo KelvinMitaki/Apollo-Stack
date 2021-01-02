a3d71d04554db182376fed1910e6b2a7db19b0e6
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-02 17:51:58 +0300

    scaffolded Listing

--- components/listing/Listing.tsx
+++ components/listing/Listing.tsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import styles from "../../styles/listingEdit.module.css";
 const Listing = () => {
-  return <div>Listing Listing</div>;
+  return (
+    <div className={styles.Listing}>
+      <div>
+        <label>list no</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>reference</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>category</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>location</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>street address</label>
+        <input type="text" />
+      </div>
+    </div>
+  );
 };
 
 export default Listing;
