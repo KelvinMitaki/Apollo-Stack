5b6d274726ce2c96cf6a019b0cc6b91dd1b38b7e
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-03 01:07:28 +0300

    added TextArea

---
+++ components/RegisterLogin/TextArea.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { WrappedFieldProps } from "redux-form";
+import styles from "../../styles/registerLoginModal.module.css";
+
+interface Props {
+  label: string;
+}
+
+const TextArea: React.FC<WrappedFieldProps & Props> = props => {
+  return (
+    <div
+      className={`${styles.input} ${
+        props.meta.error && props.meta.touched ? styles.error : ""
+      }`}
+    >
+      <label>{props.label}</label>
+      <textarea
+        {...props.input}
+        id={props.input.name}
+        cols={30}
+        rows={10}
+      ></textarea>
+      {props.meta.error && props.meta.touched && <div>{props.meta.error}</div>}
+    </div>
+  );
+};
+
+export default TextArea;
