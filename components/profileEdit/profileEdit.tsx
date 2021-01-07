0d7630a08e3a3a94c37622339fc1a9476bd8ada5
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-07 16:48:30 +0300

    refactored ProfileEdit to use rf

--- components/profileEdit/profileEdit.tsx
+++ components/profileEdit/profileEdit.tsx
@@ -1,67 +1,42 @@
 import React, { useState } from "react";
+import { Field, InjectedFormProps, reduxForm } from "redux-form";
 import styles from "../../styles/edit.module.css";
+import ProfileInput from "./ProfileInput";
 
-const ProfileEdit = () => {
-  const [input, setInput] = useState({
-    firstName: false,
-    lastName: false,
-    email: false,
-    phoneNumber: false
-  });
-  const inputFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    setInput({ ...input, [e.target.name]: true });
-  };
-  const inputBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    setInput({ ...input, [e.target.name]: false });
-  };
+const ProfileEdit: React.FC<InjectedFormProps> = props => {
   return (
     <div className={styles.edit}>
       <h4>Profile</h4>
       <form>
-        <div className={input.firstName ? styles.focused : ""}>
-          <label htmlFor="firstName">First Name</label>
-          <input
-            onFocus={inputFocus}
-            onBlur={inputBlur}
-            type="text"
-            id="firstName"
-            name="firstName"
-          />
-        </div>
-        <div className={input.lastName ? styles.focused : ""}>
-          <label htmlFor="lastName">Last Name</label>
-          <input
-            onFocus={inputFocus}
-            onBlur={inputBlur}
-            type="text"
-            id="lastName"
-            name="lastName"
-          />
-        </div>
-        <div className={input.email ? styles.focused : ""}>
-          <label htmlFor="email">Email</label>
-          <input
-            onFocus={inputFocus}
-            onBlur={inputBlur}
-            type="text"
-            id="email"
-            name="email"
-          />
-        </div>
-        <div className={input.phoneNumber ? styles.focused : ""}>
-          <label htmlFor="phoneNumber">Phone Number</label>
-          <input
-            onFocus={inputFocus}
-            onBlur={inputBlur}
-            type="text"
-            id="phoneNumber"
-            name="phoneNumber"
-          />
-        </div>
+        <Field
+          component={ProfileInput}
+          label="First Name"
+          type="text"
+          name="firstName"
+        />
+        <Field
+          component={ProfileInput}
+          label="Last Name"
+          type="text"
+          name="lastName"
+        />
+        <Field
+          component={ProfileInput}
+          label="Email"
+          type="text"
+          name="email"
+        />
+        <Field
+          component={ProfileInput}
+          label="Phone Number"
+          type="text"
+          name="phoneNumber"
+        />
+
         <button type="submit">Save</button>
       </form>
     </div>
   );
 };
 
-export default ProfileEdit;
+export default reduxForm({ form: "ProfileEdit" })(ProfileEdit);
