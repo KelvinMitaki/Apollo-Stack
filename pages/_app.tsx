dadbf41a244329333ef98dad5435047fa2e87ae6
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-07 06:57:10 +0300

    removed fetch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -63,7 +63,6 @@ export default withApollo(({ initialState, headers }) => {
           ? "https://apollo-stack-server.herokuapp.com/graphql"
           : "https://apollo-stack-server.herokuapp.com/graphql",
       credentials: "include",
-      fetch,
       fetchOptions: {
         agent: new Agent({ rejectUnauthorized: false })
       }
