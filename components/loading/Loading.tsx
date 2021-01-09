1c842f3a16cdfa88732a0b9ae8db9001fa848d10
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-09 15:15:30 +0300

    fixed modal on loading

--- components/loading/Loading.tsx
+++ components/loading/Loading.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { ActionTypes } from "../../redux/types/types";
 import styles from "../../styles/loading.module.css";
+import { SetToggleLogin } from "../Layout/Layout";
 
 const Loading = () => {
-  return <div className={styles.container}></div>;
+  const dispatch = useDispatch();
+  return (
+    <div
+      className={styles.container}
+      onClick={() =>
+        dispatch<SetToggleLogin>({
+          type: ActionTypes.toggleLogin,
+          payload: true
+        })
+      }
+    ></div>
+  );
 };
 
 export default Loading;
