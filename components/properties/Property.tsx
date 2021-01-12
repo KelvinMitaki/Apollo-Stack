f9a8ddfd48b372e7645c8db88d336c183a0ebda4
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-12 05:42:59 +0300

    overflow hidden to description

--- components/properties/Property.tsx
+++ components/properties/Property.tsx
@@ -29,7 +29,32 @@ const Property = () => {
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
               unde repudiandae enim. Blanditiis voluptate beatae nulla. Quis
               ipsa molestias quaerat libero, at quasi ipsam nesciunt nisi eum
-              odio tempore eveniet!
+              odio tempore eveniet! Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Voluptatibus labore aspernatur similique rem
+              enim a praesentium debitis necessitatibus, voluptatum, aliquid
+              eveniet perferendis officia aliquam tempore! Esse recusandae
+              facere corrupti tempore impedit necessitatibus earum, culpa
+              commodi quisquam, enim reprehenderit possimus, repudiandae vel
+              accusamus. Sequi officiis minima, ratione eligendi mollitia
+              molestias esse accusamus delectus illum quaerat quae perferendis
+              ullam soluta eum modi. Similique quibusdam deleniti dignissimos
+              nesciunt ipsa est voluptate hic, praesentium, commodi quia
+              reprehenderit. Facere fuga reprehenderit, necessitatibus molestias
+              tempora dolor alias voluptatibus, officiis dignissimos iure
+              voluptates recusandae, harum similique perferendis ipsa tempore!
+              Officiis sed et dolor expedita! Aliquid esse quisquam nobis
+              officiis in repellendus quibusdam nihil asperiores non, minus
+              magni dicta quia? Fuga vero nostrum vitae totam assumenda placeat
+              voluptate eius qui? Tenetur cum hic ut animi soluta neque esse,
+              necessitatibus molestias provident error magnam laudantium. Nihil,
+              magni aspernatur. Odit sapiente reprehenderit voluptatum explicabo
+              inventore necessitatibus eligendi repellat neque veniam eveniet.
+              Tempora accusamus deserunt totam nostrum! Laudantium temporibus,
+              sit placeat autem voluptas blanditiis unde itaque velit deserunt
+              sunt provident ipsam vel adipisci libero nisi quos non quam
+              perspiciatis a, quo praesentium est aperiam nemo ullam! Pariatur
+              molestias vel unde voluptas. Nemo aliquid delectus quas culpa,
+              consectetur nam dolorum necessitatibus earum.
             </div>
             <div className={styles.p_footer}>
               <div>
