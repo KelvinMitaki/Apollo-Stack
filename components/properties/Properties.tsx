4ce1edee39710f97062e3d3c9298cb444479a494
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-15 15:54:11 +0300

    complex pagination

--- components/properties/Properties.tsx
+++ components/properties/Properties.tsx
@@ -21,17 +21,26 @@ interface Props {
 const Properties: React.FC<Props> = props => {
   const [selectedNum, setSelectedNum] = useState<number>(1);
   let nums = [1, 2, 3, 4, 5, 6];
+  const lastPage = 200 / 10;
   if (selectedNum > 3) {
     nums = [
       selectedNum - 2,
       selectedNum - 1,
       selectedNum,
       selectedNum + 1,
-      selectedNum + 2,
-      selectedNum + 3
+      selectedNum + 2
     ];
   }
-  const lastPage = 200 / 10;
+  if (selectedNum === lastPage) {
+    nums = [
+      selectedNum - 5,
+      selectedNum - 4,
+      selectedNum - 3,
+      selectedNum - 2,
+      selectedNum - 1,
+      selectedNum
+    ];
+  }
   return (
     <div className={styles.properties_prt}>
       <h3>Property</h3>
@@ -41,16 +50,38 @@ const Properties: React.FC<Props> = props => {
         ))}
       </div>
       <div className={styles.pagination}>
-        {selectedNum > 3 && <p onClick={() => setSelectedNum(1)}>1</p>}
+        {selectedNum > 3 && (
+          <>
+            <p
+              className={selectedNum === 1 ? styles.active : ""}
+              onClick={() => setSelectedNum(1)}
+            >
+              1
+            </p>
+            <span>...</span>
+          </>
+        )}
         {nums
           .filter(n => n <= lastPage)
           .map(n => (
-            <p key={n} onClick={() => setSelectedNum(n)}>
+            <p
+              className={selectedNum === n ? styles.active : ""}
+              key={n}
+              onClick={() => setSelectedNum(n)}
+            >
               {n}
             </p>
           ))}
         {selectedNum !== lastPage && !nums.find(n => n === lastPage) && (
-          <p onClick={() => setSelectedNum(lastPage)}>{lastPage}</p>
+          <>
+            <span>...</span>{" "}
+            <p
+              className={selectedNum === lastPage ? styles.active : ""}
+              onClick={() => setSelectedNum(lastPage)}
+            >
+              {lastPage}
+            </p>
+          </>
         )}
       </div>
     </div>
