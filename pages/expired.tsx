73eb02855304280452519d2d23f61e5d7824e735
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-20 13:24:34 +0300

    wired up fetching expired listings

--- pages/expired.tsx
+++ pages/expired.tsx
@@ -1,32 +1,32 @@
+import { useQuery } from "@apollo/client";
+import { NextPage } from "next";
 import React, { useState } from "react";
 import { BiCheck } from "react-icons/bi";
 import { FiCheck } from "react-icons/fi";
+import { initializeApollo } from "../apollo";
 import ExpiredListing from "../components/Expired/ExpiredListing";
 import ExpiredMobileListing from "../components/Expired/ExpiredMobileListing";
 import HouseFilter from "../components/Homepage/Header/HouseFilter";
 import Layout from "../components/Layout/Layout";
+import { ListingProperty } from "../components/listings/Listing";
+import {
+  EXPIRED_LISTINGS_COUNT,
+  FETCH_EXPIRED_LISTINGS
+} from "../graphql/queries/queries";
 import withAgent from "../HOCs/withAgent";
 import styles from "../styles/listings.module.css";
 
-const expired = () => {
+const expired: NextPage = () => {
   const [check, setCheck] = useState<boolean>(false);
   const [checkExpired, setCheckExpired] = useState<boolean>(false);
+  const { data } = useQuery(FETCH_EXPIRED_LISTINGS, {
+    fetchPolicy: "cache-only"
+  });
+  const countData = useQuery(EXPIRED_LISTINGS_COUNT, {
+    fetchPolicy: "cache-only"
+  });
+  console.log(data);
 
-  const listings = [];
-  for (let i = 0; i < 50; i++) {
-    listings.push(
-      <ExpiredListing
-        key={i}
-        className={`${i % 2 === 0 ? "active" : ""}`}
-        checked={check}
-        setCheckExpired={setCheckExpired}
-      />
-    );
-  }
-  const mobileListings = [];
-  for (let i = 0; i < 50; i++) {
-    mobileListings.push(<ExpiredMobileListing key={i} />);
-  }
   return (
     <Layout title="Expired Listings">
       <div className={styles.container}>
@@ -104,13 +104,57 @@ const expired = () => {
                 </th>
               </tr>
             </thead>
-            <tbody>{listings}</tbody>
+            <tbody>
+              {(data.fetchExpiredListings as ListingProperty[])
+                .filter((l, i, s) => s.findIndex(pr => pr._id === l._id) === i)
+                .map((prop, i) => (
+                  <ExpiredListing
+                    key={i}
+                    className={`${i % 2 === 0 ? "active" : ""}`}
+                    checked={check}
+                    setCheckExpired={setCheckExpired}
+                  />
+                ))}
+            </tbody>
           </table>
-          {mobileListings}
+          {(data.fetchExpiredListings as ListingProperty[])
+            .filter((l, i, s) => s.findIndex(pr => pr._id === l._id) === i)
+            .map((prop, i) => (
+              <ExpiredMobileListing key={i} />
+            ))}
         </div>
       </div>
     </Layout>
   );
 };
-
+expired.getInitialProps = async ctx => {
+  try {
+    const apolloClient = initializeApollo();
+    await apolloClient.query({
+      query: FETCH_EXPIRED_LISTINGS,
+      variables: { offset: 0, limit: 10 },
+      context: {
+        headers: {
+          cookie: ctx.req?.headers.cookie
+        }
+      }
+    });
+    await apolloClient.query({
+      query: EXPIRED_LISTINGS_COUNT,
+      context: {
+        headers: {
+          cookie: ctx.req?.headers.cookie
+        }
+      }
+    });
+    return {
+      initialApolloState: apolloClient.cache.extract()
+    };
+  } catch (error) {
+    if (ctx.res) {
+      ctx.res.writeHead(301, { Location: "/" });
+      ctx.res.end();
+    }
+  }
+};
 export default withAgent(expired);
