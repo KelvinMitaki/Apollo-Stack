d07cc5e487ec08226ccac29e14d04b2caeb07d9e
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-22 23:56:35 +0300

    wired up views and leads

--- pages/agency/statistics.tsx
+++ pages/agency/statistics.tsx
@@ -60,10 +60,10 @@ const statistics: NextPage = () => {
     }[]).find(d => d.month === month);
     return d ? d.count.toLocaleString() : "-";
   };
-  const sortMonths = (month: number) => {
+  const sortMonths = (month: number, determinant: "leads" | "views") => {
     const date = new Date();
     date.setMonth(date.getMonth() - month);
-    const d = (data.countViewsAndLeadsCount.leads as {
+    const d = (data.countViewsAndLeadsCount[determinant] as {
       month: string;
       count: number;
     }[]).find(d => d.month === months[date.getMonth()]);
@@ -109,59 +109,66 @@ const statistics: NextPage = () => {
             </tr>
           </tbody>
         </table>
-        <AreaChart
-          width={730}
-          height={250}
-          data={[
-            sortMonths(6),
-            sortMonths(5),
-            sortMonths(4),
-            sortMonths(3),
-            sortMonths(2),
-            sortMonths(1),
-            sortMonths(0)
-          ]}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-        >
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis dataKey="month" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="count"
-            stroke="#8884d8"
-            fillOpacity={1}
-            fill="url(#colorUv)"
-          />
-        </AreaChart>
-        <BarChart
-          width={730}
-          height={250}
-          data={[
-            sortMonths(6),
-            sortMonths(5),
-            sortMonths(4),
-            sortMonths(3),
-            sortMonths(2),
-            sortMonths(1),
-            sortMonths(0)
-          ]}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="month" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="count" fill="#8884d8" />
-          {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
-        </BarChart>
+        <div className={styles.views_data}>
+          <h4>Views</h4>
+          <AreaChart
+            width={730}
+            height={250}
+            data={[
+              sortMonths(6, "views"),
+              sortMonths(5, "views"),
+              sortMonths(4, "views"),
+              sortMonths(3, "views"),
+              sortMonths(2, "views"),
+              sortMonths(1, "views"),
+              sortMonths(0, "views")
+            ]}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="month" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="count"
+              stroke="#8884d8"
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+          </AreaChart>
+        </div>
+        <div className={styles.leads_data}>
+          <h4>Leads</h4>
+
+          <BarChart
+            width={730}
+            height={250}
+            data={[
+              sortMonths(6, "leads"),
+              sortMonths(5, "leads"),
+              sortMonths(4, "leads"),
+              sortMonths(3, "leads"),
+              sortMonths(2, "leads"),
+              sortMonths(1, "leads"),
+              sortMonths(0, "leads")
+            ]}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" fill="#8884d8" />
+            {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
+          </BarChart>
+        </div>
       </div>
     </Layout>
   );
