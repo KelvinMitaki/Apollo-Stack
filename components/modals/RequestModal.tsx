604d800ae90987d10aeec2cc3bc0e049d7069304
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-25 16:09:39 +0300

    added ref to RequestModal

--- components/modals/RequestModal.tsx
+++ components/modals/RequestModal.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { MdCancel } from "react-icons/md";
 import styles from "../../styles/requestModal.module.css";
 
 const RequestModal: React.FC = () => {
+  const divRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+  const handleClickOutside = (e: Event) => {
+    // @ts-ignore
+    if (divRef.current && !divRef.current.contains(e.target)) {
+    }
+  };
   return (
     <div className={styles.prt}>
-      <div className={styles.container}>
+      <div className={styles.container} ref={divRef}>
         <div className={styles.header}>
           <p>request</p>
           <MdCancel size="2rem" />
