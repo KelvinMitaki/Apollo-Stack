08578797a8fa6d1596c95e17201c1c5d72ed9cdf
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-25 13:39:37 +0300

    formatted time on ContactFormMessages

--- components/listing/SingleListingBody.tsx
+++ components/listing/SingleListingBody.tsx
@@ -9,18 +9,15 @@ interface Props {
 
 const SingleListingBody: React.FC<Props> = props => {
   const {
-    contactFormMessage: {
-      _id,
-      createdAt,
-      email,
-      fullName,
-      message,
-      phoneNumber
-    }
+    contactFormMessage: { createdAt, email, fullName, message, phoneNumber }
   } = props;
   return (
     <tr className={`${props.active ? styles.active : ""} ${styles.msg_row}`}>
-      <td>{new Date(createdAt).toLocaleDateString()}</td>
+      <td>
+        {new Date(createdAt).toDateString()}{" "}
+        <span style={{ textTransform: "lowercase" }}>at</span>{" "}
+        {new Date(createdAt).toLocaleTimeString()}
+      </td>
       <td>{fullName}</td>
       <td className={styles.email}>{email}</td>
       <td>07{phoneNumber.toString().slice(-8)}</td>
