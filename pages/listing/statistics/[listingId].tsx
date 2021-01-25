02a17486c683b65435cf936d81eea77814b15edb
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-25 13:14:15 +0300

    added pagination to singleListing

--- pages/listing/statistics/[listingId].tsx
+++ pages/listing/statistics/[listingId].tsx
@@ -1,9 +1,12 @@
+import { useQuery } from "@apollo/client";
 import { NextPage } from "next";
-import React from "react";
+import React, { useState } from "react";
 import { initializeApollo } from "../../../apollo";
 import Layout from "../../../components/Layout/Layout";
 import ContactFormMessages from "../../../components/listing/ContactFormMessages";
 import SingleListingGraphs from "../../../components/listing/SingleListingGraphs";
+import Loading from "../../../components/loading/Loading";
+import Pagination from "../../../components/properties/Pagination";
 import {
   PROPERTY_STATISTICS,
   PROPERTY_STATISTICS_MESSAGES,
@@ -13,15 +16,69 @@ import withAgent from "../../../HOCs/withAgent";
 import styles from "../../../styles/singleListing.module.css";
 
 interface Props {
-  variables: { _id: string | string[] | undefined };
+  variables: { _id: string | string[] | undefined; offset: 0; limit: 10 };
 }
 
 const singleListing: NextPage<Props> = props => {
+  const [limit, setLimit] = useState<number>(10);
+  const [skip, setSkip] = useState<number>(0);
+  const [selectedNum, setSelectedNum] = useState<number>(1);
+  const { data, fetchMore, loading } = useQuery(PROPERTY_STATISTICS_MESSAGES, {
+    fetchPolicy: "cache-only",
+    variables: { ...props.variables, offset: skip, limit },
+    notifyOnNetworkStatusChange: true,
+    onError(err) {
+      console.log(err);
+      console.log(err.graphQLErrors);
+      console.log(err.message);
+    }
+  });
+  const countData = useQuery(PROPERTY_STATISTICS_MESSAGES_COUNT, {
+    fetchPolicy: "cache-only",
+    variables: props.variables
+  });
+  let nums = [1, 2, 3, 4, 5, 6];
+  const lastPage = Math.ceil(
+    countData.data.propertyStatisticsMessagesCount.count / 10
+  );
+  if (selectedNum > 3) {
+    nums = [
+      selectedNum - 2,
+      selectedNum - 1,
+      selectedNum,
+      selectedNum + 1,
+      selectedNum + 2
+    ];
+  }
+  if (selectedNum === lastPage) {
+    nums = [
+      selectedNum - 5,
+      selectedNum - 4,
+      selectedNum - 3,
+      selectedNum - 2,
+      selectedNum - 1,
+      selectedNum
+    ];
+  }
+  if (nums.find(num => num < 1)) {
+    nums = nums.filter(num => num > 0);
+  }
   return (
     <Layout title="Lavington, Nairobi">
       <div className={styles.container}>
+        {loading && <Loading />}
         <SingleListingGraphs variables={props.variables} />
         <ContactFormMessages />
+        <Pagination
+          setLimit={setLimit}
+          nums={nums}
+          lastPage={lastPage}
+          fetchMore={fetchMore}
+          properties={data.fetchAgentProperties}
+          selectedNum={selectedNum}
+          setSelectedNum={setSelectedNum}
+          setSkip={setSkip}
+        />
       </div>
     </Layout>
   );
@@ -68,7 +125,7 @@ singleListing.getInitialProps = async ctx => {
       ctx.res.end();
     }
     return {
-      variables: { _id: ctx.query.listingId }
+      variables: { _id: ctx.query.listingId, offset: 0, limit: 10 }
     };
   }
 };
